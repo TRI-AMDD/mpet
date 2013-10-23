e3815ea082245bf0115a141facb00b1ec90da9b2
Raymond Smith <raymondbarrettsmith@gmail.com>
2013-10-23 12:54:11 -0400

    mpet.py: Enhance plot options in plot.py
    Plot separators on electrolyte plots, add electrolyte potential, add
    average solid concentrations (sort-of).

--- plot.py
+++ plot.py
@@ -14,7 +14,13 @@ def plot(plot_type, save_flag):
     infile = "acr_sp_0.001C.mat"
 #    infile = "acr_sp.mat"
     data = sio.loadmat(infile)
+#    print 1./data['mpet.currset']
 #    print data.keys()
+#    print data['mpet.poros_sep']
+#    print data['mpet.poros_trode']
+#    print data['mpet.currset']
+#    print data['mpet.tp']
+#    print data['mpet.dim_Damb']
 #    print data['mpet.c_lyte_sep']
 #    print data['mpet.c_lyte_trode']
 #    print data['mpet.phi_applied']
@@ -51,77 +57,44 @@ def plot(plot_type, save_flag):
         ax.plot(times, voltage)
         plt.show()
 
-    # Plot concentration profiles
-    elif plot_type == "c":
+    # Plot electrolyte concentration
+    elif plot_type == "elytec":
         if save_flag:
             outputdir = "{name}_image_output".format(name=infile[0:-4])
             os.mkdir(outputdir)
         else:
             # "interactive mode on"
             plt.ion()
-#        numtimes = len(data['mpet.phi_applied_times'][0])
-        fig = plt.figure()
-#        ax = fig.add_axes([0.08, 0.08, 0.85, 0.85])
-        ax = fig.add_subplot(111)
-        ax.set_ylim((0, 1))
-        ax.set_xlabel('Particle Length [nm]')
-        ax.set_ylabel('Concentration [fraction]')
-        sol = 'mpet.solid_vol9_part0'
-        datay = data[sol][0]
-#        print datay
-#        Lx = data['mpet.Lx'][0]
-        Lx = 1.
-        numy = len(datay)
-        datax = np.linspace(0, Lx, numy)
-        # returns tuble of line objects, thus comma
-        line1, = ax.plot(datay)
-#        for i in range(5):
-        for i in range(numtimes):
-#            print "img {num}/{tot}".format(num=i, tot=numtimes)
-            datay = data[sol][i]
-            line1.set_ydata(datay)
-            if save_flag:
-                filename = os.path.join(outputdir,
-                        'c{num:06d}.png'.format(num=i))
-                fig.savefig(filename)
-            else:
-                fig.canvas.draw()
-                time.sleep(1e-3)
-#            fig.clear()
-
-    elif plot_type == "e":
-        if save_flag:
-            outputdir = "{name}_image_output".format(name=infile[0:-4])
-            os.mkdir(outputdir)
-        else:
-            # "interactive mode on"
-            plt.ion()
-#        numtimes = len(data['mpet.phi_applied_times'][0])
-#        numtimes = len(times)
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.set_ylim((0, 1))
+        ymin = 0
+        ymax = 1.5
         ax.set_xlabel('Battery Position [um]')
         ax.set_ylabel('Concentration of electrolyte [fraction]')
         sep = 'mpet.c_lyte_sep'
         trode = 'mpet.c_lyte_trode'
         datay_sep = data[sep][0]
         datay_trode = data[trode][0]
-        print datay_sep
-        print datay_trode
+#        print datay_sep
+#        print datay_trode
         Lsep = data['mpet.Lsep'][0] * 1e6
         Ltrode = data['mpet.Ltrode'][0] * 1e6
         datay = np.hstack((datay_sep, datay_trode))
-        print datay.shape
+#        print datay.shape
         numy = len(datay)
-        datax = np.linspace(0, (Lsep + Ltrode), numy)
+        xmin = 0
+        xmax = Lsep + Ltrode
+        datax = np.linspace(xmin, xmax, numy)
+        ax.set_ylim((ymin, ymax))
+        ax.set_xlim((xmin, xmax))
         # returns tuble of line objects, thus comma
         line1, = ax.plot(datax, datay)
+        ax.axvline(x=Lsep, ymin=ymin, ymax=ymax, linestyle='--', color='g')
         for i in range(numtimes):
             datay_sep = data[sep][i]
             datay_trode = data[trode][i]
-            print datay_sep
-            print datay_trode
+#            print datay_sep
+#            print datay_trode
             datay = np.hstack((datay_sep, datay_trode))
 #            print datay.shape
             line1.set_ydata(datay)
@@ -133,6 +106,56 @@ def plot(plot_type, save_flag):
                 fig.canvas.draw()
                 time.sleep(1e-3)
 
+    # Plot electrolyte electrostatic potential
+    elif plot_type == "elytep":
+        if save_flag:
+            outputdir = "{name}_image_output".format(name=infile[0:-4])
+            os.mkdir(outputdir)
+        else:
+            # "interactive mode on"
+            plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ymin = -5
+        ymax = 5
+        ax.set_xlabel('Battery Position [um]')
+        ax.set_ylabel('Potential of electrolyte [fraction]')
+        sep = 'mpet.phi_lyte_sep'
+        trode = 'mpet.phi_lyte_trode'
+        datay_sep = data[sep][0]
+        datay_trode = data[trode][0]
+#        print datay_sep
+#        print datay_trode
+        Lsep = data['mpet.Lsep'][0] * 1e6
+        Ltrode = data['mpet.Ltrode'][0] * 1e6
+        datay = np.hstack((datay_sep, datay_trode))
+#        print datay.shape
+        numy = len(datay)
+        xmin = 0
+        xmax = Lsep + Ltrode
+        datax = np.linspace(xmin, xmax, numy)
+        ax.set_ylim((ymin, ymax))
+        ax.set_xlim((xmin, xmax))
+        # returns tuble of line objects, thus comma
+        line1, = ax.plot(datax, datay)
+        ax.axvline(x=Lsep, ymin=ymin, ymax=ymax, linestyle='--', color='g')
+        for i in range(numtimes):
+            datay_sep = data[sep][i]
+            datay_trode = data[trode][i]
+#            print datay_sep
+#            print datay_trode
+            datay = np.hstack((datay_sep, datay_trode))
+#            print datay.shape
+            line1.set_ydata(datay)
+            if save_flag:
+                filename = os.path.join(outputdir,
+                        'c{num:06d}.png'.format(num=i))
+                fig.savefig(filename)
+            else:
+                fig.canvas.draw()
+                time.sleep(1e-3)
+
+    # Plot average solid concentrations
     elif plot_type == "csld":
         if save_flag:
             outputdir = "{name}_image_output".format(name=infile[0:-4])
@@ -151,10 +174,14 @@ def plot(plot_type, save_flag):
                 sol[i, j] = "mpet.solid_vol{j}_part{i}".format(i=i, j=j)
 #                print data["mpet.psd_lengths"]
                 lens[i, j] = data["mpet.psd_lengths"][0][j, i]
+                # Remove axis ticks
+                ax[i, j].xaxis.set_major_locator(plt.NullLocator())
+#                ax[i, j].yaxis.set_major_locator(plt.NullLocator())
+                # OR
 #                ax[i, j].set_frame_on(False)
-                ax[i, j].xaxis.set_ticks_position('none')
+#                ax[i, j].xaxis.set_ticks_position('none')
+#                ax[i, j].xaxis.set_ticklabels('')
 #                ax[i, j].yaxis.set_ticks_position('none')
-                ax[i, j].xaxis.set_ticklabels('')
 #                ax[i, j].yaxis.set_ticklabels('')
                 datay = data[sol[i, j]][0]
                 numy = len(datay)
@@ -176,9 +203,41 @@ def plot(plot_type, save_flag):
                 fig.canvas.draw()
                 time.sleep(1e-3)
 
+    # Plot all solid concentrations
+    elif plot_type == "cbar":
+        if save_flag:
+            outputdir = "{name}_image_output".format(name=infile[0:-4])
+            os.mkdir(outputdir)
+        else:
+            # "interactive mode on"
+            plt.ion()
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        ax.patch.set_facecolor('gray')
+        ax.set_aspect('equal', 'box')
+        ax.xaxis.set_major_locator(plt.NullLocator())
+        ax.yaxis.set_major_locator(plt.NullLocator())
+        for tval in range(numtimes):
+            cbar_mat = data['mpet.cbar_sld'][tval]
+            for (x,y),w in np.ndenumerate(cbar_mat):
+                color = 'white' if w > 0 else 'black'
+                size = np.sqrt(np.abs(w))
+                rect = plt.Rectangle([x - size / 2, y - size / 2],
+                        size, size, facecolor=color, edgecolor=color)
+                ax.add_patch(rect)
+            ax.autoscale_view()
+            ax.invert_yaxis()
+            if save_flag:
+                filename = os.path.join(outputdir,
+                        'c{num:06d}.png'.format(num=i))
+                fig.savefig(filename)
+            else:
+                fig.canvas.draw()
+                time.sleep(1e-3)
+
     else:
         raise Exception("Unexpected plot type argument."
-                + "Try 'v' or 'c' or 'csld'")
+                + "Try 'v' or 'elyte' or 'cbar' or 'csld'")
     return
 
 if __name__ == "__main__":
