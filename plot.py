1a0fd90c49e25ca3c09265cdb1cc0b4aa3cc40f1
Raymond Smith <raymondbarrettsmith@gmail.com>
2013-11-08 09:57:54 -0500

    plot.py: All animations use FuncAnimation now.
    Also, cbar should still be fast, and creating the movie should be
    taken care of all in this script rather than involving another step.

--- plot.py
+++ plot.py
@@ -7,15 +7,17 @@ import scipy.io as sio
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import matplotlib.animation as manim
+import matplotlib.collections as mcollect
 
-def plot(infile, plot_type, save_flag):
+def show_data(infile, plot_type, save_flag):
     pfx = 'mpet.'
     data = sio.loadmat(infile)
     # Pick out some useful parameters
-    Vstd = float(data[pfx + 'Vstd'][0][0])        # Standard potential, V
-    k = float(data[pfx + 'k'][0][0])       # Boltzmann constant
-    T = float(data[pfx + 'Tabs'][0][0])             # Temp, K
-    e = float(data[pfx + 'e'][0][0])       # Charge of proton, C
+    Vstd = data[pfx + 'Vstd'][0][0]  # Standard potential, V
+    k = data[pfx + 'k'][0][0]        # Boltzmann constant
+    T = data[pfx + 'Tabs'][0][0]     # Temp, K
+    e = data[pfx + 'e'][0][0]        # Charge of proton, C
     # Discretization info
     Ntrode = int(data[pfx + 'NumTrode'][0][0])
     Nsep = int(data[pfx + 'NumSep'][0][0])
@@ -30,38 +32,36 @@ def plot(infile, plot_type, save_flag):
     shape_C3 = data[pfx + "shape_C3"][0][0]
 
     # Print relevant simulation info
-#    if plot_type == "params":
-    if True:
-        if sim_ACR:
-            print "Type: ACR"
-        elif sim_homog:
-            print "Type: homog"
-        else:
-            print "Type: ?"
-        if shape_sphere:
-            print "Shape: sphere"
-        elif shape_C3:
-            print "Shape: C3"
-        else:
-            print "Shape: ?"
-        print "C_rate:", data[pfx + "C_rate"][0][0]
-        print "psd_mean [nm]:", data[pfx + "psd_mean"][0][0]*1e9
-        print "psd_stddev [nm]:", data[pfx + "psd_stddev"][0][0]*1e9
-        print "Nsep:", Nsep
-        print "Ntrode:", Ntrode
-        print "Npart:", numpart
-        print "dim_Dp [m^2/s]:", data[pfx + "dim_Dp"][0][0]
-        print "dim_Dm [m^2/s]:", data[pfx + "dim_Dm"][0][0]
-        print "dim_Damb [m^2/s]:", data[pfx + "dim_Damb"][0][0]
-        print "alpha:", data[pfx + "alpha"][0][0]
-        print "dim_k0 [A/m^2]:", data[pfx + "dim_k0"][0][0]
-        mcond_bool = data[pfx + "cath_bulk_cond"][0][0]
-        if mcond_bool:
-            print ("cathode conductivity loss: Yes -- " +
-                    "dim_mcond [S/m]: " +
-                    str(data[pfx + "dim_mcond"][0][0]))
-        else:
-            print "cathode conductivity loss: No"
+    if sim_ACR:
+        print "Type: ACR"
+    elif sim_homog:
+        print "Type: homog"
+    else:
+        print "Type: ?"
+    if shape_sphere:
+        print "Shape: sphere"
+    elif shape_C3:
+        print "Shape: C3"
+    else:
+        print "Shape: ?"
+    print "C_rate:", data[pfx + "C_rate"][0][0]
+    print "psd_mean [nm]:", data[pfx + "psd_mean"][0][0]*1e9
+    print "psd_stddev [nm]:", data[pfx + "psd_stddev"][0][0]*1e9
+    print "Nsep:", Nsep
+    print "Ntrode:", Ntrode
+    print "Npart:", numpart
+    print "dim_Dp [m^2/s]:", data[pfx + "dim_Dp"][0][0]
+    print "dim_Dm [m^2/s]:", data[pfx + "dim_Dm"][0][0]
+    print "dim_Damb [m^2/s]:", data[pfx + "dim_Damb"][0][0]
+    print "alpha:", data[pfx + "alpha"][0][0]
+    print "dim_k0 [A/m^2]:", data[pfx + "dim_k0"][0][0]
+    mcond_bool = data[pfx + "cath_bulk_cond"][0][0]
+    if mcond_bool:
+        print ("cathode conductivity loss: Yes -- " +
+                "dim_mcond [S/m]: " +
+                str(data[pfx + "dim_mcond"][0][0]))
+    else:
+        print "cathode conductivity loss: No"
 
     # Plot voltage profile
     if plot_type == "v":
@@ -72,16 +72,13 @@ def plot(infile, plot_type, save_flag):
         ax.plot(ffvec, voltage)
         ax.set_xlabel("Cathode Filling Fraction [dimensionless]")
         ax.set_ylabel("Voltage [V]")
+        if save_flag:
+            fig.savefig("mpet_v.png")
         plt.show()
+        return
 
     # Plot electrolyte concentration
     elif plot_type == "elytec":
-        if save_flag:
-            outputdir = "{name}_image_output".format(name=infile[0:-4])
-            os.mkdir(outputdir)
-        else:
-            # "interactive mode on"
-            plt.ion()
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ymin = 0
@@ -104,27 +101,18 @@ def plot(infile, plot_type, save_flag):
         # returns tuble of line objects, thus comma
         line1, = ax.plot(datax, datay)
         ax.axvline(x=Lsep, ymin=ymin, ymax=ymax, linestyle='--', color='g')
-        for i in range(numtimes):
-            datay_sep = data[sep][i]
-            datay_trode = data[trode][i]
+        def init():
+            line1.set_ydata(np.ma.array(datax, mask=True))
+            return line1,
+        def animate(tind):
+            datay_sep = data[sep][tind]
+            datay_trode = data[trode][tind]
             datay = np.hstack((datay_sep, datay_trode))
             line1.set_ydata(datay)
-            if save_flag:
-                filename = os.path.join(outputdir,
-                        'elyte_c{num:06d}.png'.format(num=i))
-                fig.savefig(filename)
-            else:
-                fig.canvas.draw()
-                time.sleep(1e-3)
+            return line1,
 
     # Plot electrolyte electrostatic potential
     elif plot_type == "elytep":
-        if save_flag:
-            outputdir = "{name}_image_output".format(name=infile[0:-4])
-            os.mkdir(outputdir)
-        else:
-            # "interactive mode on"
-            plt.ion()
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ymin = -5
@@ -147,27 +135,18 @@ def plot(infile, plot_type, save_flag):
         # returns tuble of line objects, thus comma
         line1, = ax.plot(datax, datay)
         ax.axvline(x=Lsep, ymin=ymin, ymax=ymax, linestyle='--', color='g')
-        for i in range(numtimes):
-            datay_sep = data[sep][i]
-            datay_trode = data[trode][i]
+        def init():
+            line1.set_ydata(np.ma.array(datax, mask=True))
+            return line1,
+        def animate(tind):
+            datay_sep = data[sep][tind]
+            datay_trode = data[trode][tind]
             datay = np.hstack((datay_sep, datay_trode))
             line1.set_ydata(datay)
-            if save_flag:
-                filename = os.path.join(outputdir,
-                        'elyte_p{num:06d}.png'.format(num=i))
-                fig.savefig(filename)
-            else:
-                fig.canvas.draw()
-                time.sleep(1e-3)
+            return line1,
 
     # Plot all solid concentrations
     elif plot_type == "csld":
-        if save_flag:
-            outputdir = "{name}_image_output".format(name=infile[0:-4])
-            os.mkdir(outputdir)
-        else:
-            # "interactive mode on"
-            plt.ion()
         fig, ax = plt.subplots(numpart, Ntrode, squeeze=False,
                 sharey=True)
         sol = np.empty((numpart, Ntrode), dtype=object)
@@ -187,41 +166,32 @@ def plot(infile, plot_type, save_flag):
                 ax[i, j].set_xlim((0, lens[i, j]))
                 line, = ax[i, j].plot(datax, datay)
                 lines[i, j] = line
-        for tval in range(numtimes):
+        def init():
             for i in range(numpart):
                 for j in range(Ntrode):
-                    datay = data[sol[i, j]][tval]
+                    datax = np.zeros(data[sol[i, j]][0].shape)
+                    lines[i, j].set_ydata(np.ma.array(datax, mask=True))
+            return tuple(lines.reshape(-1))
+        def animate(tind):
+            for i in range(numpart):
+                for j in range(Ntrode):
+                    datay = data[sol[i, j]][tind]
                     lines[i, j].set_ydata(datay)
-            if save_flag:
-                filename = os.path.join(outputdir,
-                        'c{num:06d}.png'.format(num=tval))
-                fig.savefig(filename)
-            else:
-                fig.canvas.draw()
-                time.sleep(1e-3)
+            return tuple(lines.reshape(-1))
 
     # Plot average solid concentrations
     elif plot_type == "cbar":
-        if save_flag:
-            outputdir = "{name}_image_output".format(name=infile[0:-4])
-            os.mkdir(outputdir)
-        else:
-            # "interactive mode on"
-            plt.ion()
         # Get particle sizes (and max size) (length-based)
         psd_len = data[pfx + 'psd_lengths'][0]
         len_max = np.max(psd_len)
         len_min = np.min(psd_len)
         size_min = 0.10
-        fig = plt.figure()
-        ax = fig.add_subplot(1,1,1)
+        fig, ax = plt.subplots()
 #        title = ax.set_title("% = 00.0")
 #        tmin = np.min(times)
 #        tmax = np.max(times)
-#        ax.patch.set_facecolor('gray')
         ax.patch.set_facecolor('white')
         ax.set_aspect('equal', 'box')
-#        ax.set_aspect('equal')
         ax.xaxis.set_major_locator(plt.NullLocator())
         ax.yaxis.set_major_locator(plt.NullLocator())
 #        yleft = fig.text(0.3, 0.5, "Separator",
@@ -231,13 +201,15 @@ def plot(infile, plot_type, save_flag):
 #                rotation=90,
 #                verticalalignment="center")
 #        ax.invert_yaxis()
+        # Make a collection of rectangles we'll animate
         rects = np.empty((Ntrode, numpart), dtype=object)
         cbar_mat = data[pfx + 'cbar_sld'][0]
         converter = matplotlib.colors.ColorConverter()
         rgba_green = converter.to_rgba('green')
         rgba_yellow = converter.to_rgba('yellow')
         rgba_red = converter.to_rgba('red')
-        for (i,j),c in np.ndenumerate(cbar_mat):
+        color = 'green'
+        for (i, j), c in np.ndenumerate(cbar_mat):
             p_len = psd_len[i, j]
             if len_max == len_min:
                 size_frac = 0.4
@@ -245,49 +217,34 @@ def plot(infile, plot_type, save_flag):
                 size_frac = (p_len - len_min)/(len_max - len_min)
             size = ((size_frac) * (1-size_min) +
                     size_min)
-            color = 'green'
             rects[i, j] = plt.Rectangle([i - size / 2, j - size / 2],
                     size, size, facecolor=color, edgecolor=color)
-            ax.add_patch(rects[i, j])
+        collection = mcollect.PatchCollection(rects.reshape(-1),
+                animated=True)
+        ax.add_collection(collection)
         ax.autoscale_view()
-        for tval in range(1,numtimes):
-#            t_current = times[tval]
-#            tfrac = (t_current - tmin)/(tmax - tmin) * 100
-#            ax.set_title("% = {perc:2.1f}".format(perc=tfrac))
-#            title.set_text("% = {perc:2.1f}".format(perc=tfrac))
-#            ax.draw_artist(title)
-            cbar_mat = data[pfx + 'cbar_sld'][tval]
-            for (i,j),c in np.ndenumerate(cbar_mat):
+        def init():
+            colors = 'green'
+            collection.set_edgecolors(colors)
+            collection.set_facecolors(colors)
+            return collection,
+        def animate(tind):
+            colors = []
+            cbar_mat = data[pfx + 'cbar_sld'][tind]
+            for (i,), c in np.ndenumerate(cbar_mat.reshape(-1)):
                 if c < 0.4:
-                    color = 'green'
                     color_code = rgba_green
                 elif c < 0.6:
-                    color = 'yellow'
                     color_code = rgba_yellow
-                else: # c > 0.6
-                    color = 'red'
+                else: # c > 0.6:
                     color_code = rgba_red
-                fc = rects[i, j].get_facecolor()
-                if fc != color_code:
-                    rects[i, j].set_facecolor(color)
-                    rects[i, j].set_edgecolor(color)
-                    ax.draw_artist(rects[i, j])
-            if save_flag:
-                filename = os.path.join(outputdir,
-                        'cbar{num:06d}.png'.format(num=tval))
-                fig.savefig(filename)
-            else:
-                fig.canvas.blit(ax.bbox)
-                time.sleep(1e-3)
+                colors.append(color_code)
+            collection.set_edgecolors(colors)
+            collection.set_facecolors(colors)
+            return collection,
 
     # Plot cathode potential
     elif plot_type == "cathp":
-        if save_flag:
-            outputdir = "{name}_image_output".format(name=infile[0:-4])
-            os.mkdir(outputdir)
-        else:
-            # "interactive mode on"
-            plt.ion()
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ymin = -1
@@ -305,21 +262,26 @@ def plot(infile, plot_type, save_flag):
         ax.set_xlim((xmin, xmax))
         # returns tuble of line objects, thus comma
         line1, = ax.plot(datax, datay)
-        for i in range(numtimes):
+        def init():
+            line1.set_ydata(np.ma.array(datax, mask=True))
+            return line1,
+        def animate(i):
             datay = data[cathp][i]
             line1.set_ydata(datay)
-            if save_flag:
-                filename = os.path.join(outputdir,
-                        'elyte_p{num:06d}.png'.format(num=i))
-                fig.savefig(filename)
-            else:
-                fig.canvas.draw()
-                time.sleep(1e-3)
+            return line1,
 
     else:
         raise Exception("Unexpected plot type argument. "
                 + "Try 'v', 'elytec', 'elytep', 'cbar', 'csld', "
                 + "'cathp'.")
+
+    ani = manim.FuncAnimation(fig, animate, frames=numtimes,
+            interval=10, blit=True, repeat=False, init_func=init)
+    if save_flag:
+        ani.save("mpet_{type}.mp4".format(type=plot_type), fps=30)
+#                extra_args=['-vcodec', 'libx264'])
+    plt.show()
+
     return
 
 if __name__ == "__main__":
@@ -340,4 +302,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 3:
         if sys.argv[3] == "save":
             save_flag = True
-    plot(infile, plot_type, save_flag)
+    show_data(infile, plot_type, save_flag)
