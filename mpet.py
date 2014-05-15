Fix erf derivative info. Now run MHC with erf.

mpet.py
@@ -789,15 +789,15 @@ class modMPET(daeModel):
         def knet(eta, lmbda, b):
             argox = (eta - lmbda)/b
             argrd = (-eta - lmbda)/b
-#            # MHCerf
-#            erfox = ERF("erf", self, unit(), argox)
-#            erfrd = ERF("erf", self, unit(), argrd)
-#            self.erfvec.append([erfox, erfrd])
-#            kox = Aa*(erfox() + 1)
-#            krd = Aa*(erfrd() + 1)
-            # MHCtanh
-            kox = Aa*(self.TANH(argox) + 1)
-            krd = Aa*(self.TANH(argrd) + 1)
+            # MHCerf
+            erfox = ERF("erf", self, unit(), argox)
+            erfrd = ERF("erf", self, unit(), argrd)
+            self.erfvec.append([erfox, erfrd])
+            kox = Aa*(erfox() + 1)
+            krd = Aa*(erfrd() + 1)
+#            # MHCtanh
+#            kox = Aa*(self.TANH(argox) + 1)
+#            krd = Aa*(self.TANH(argrd) + 1)
             k = krd - kox
             return k
         if type(eta) == np.ndarray:
@@ -967,8 +967,8 @@ class simMPET(daeSimulation):
             lmbda = D['lambda_ac'][l]/(k*Tref)
             self.m.lmbda_ac.SetValue(l, lmbda)
 #            MHC_b = 2*np.sqrt(self.m.lmbda_ac.GetValue(l))
-#            MHC_erf_b = 2*np.sqrt(lmbda) # MHCerf
-            MHC_erf_b = np.sqrt(lmbda*np.pi) # MHCtanh
+            MHC_erf_b = 2*np.sqrt(lmbda) # MHCerf
+#            MHC_erf_b = np.sqrt(lmbda*np.pi) # MHCtanh
             self.m.MHC_erfstretch_ac.SetValue(l, MHC_erf_b)
 #            self.m.MHC_Aa_ac.SetValue(l, )
             self.m.B_ac.SetValue(l,
@@ -999,10 +999,10 @@ class simMPET(daeSimulation):
                     k0tmp = ((p_area/p_vol)*D['k0_ac'][l]*td)/(F*csmax_ac[l])
                     self.m.k0_ac[l].SetValue(i, j, k0tmp)
 #                            ((p_area/p_vol)*D['k0_ac'][l]*td)/(F*csmax_ac[l]))
-#                    self.m.MHC_Aa_ac[l].SetValue(i, j, # MHCerf
-#                            k0tmp / (spcl.erf(-lmbda/MHC_erf_b) + 1))
-                    self.m.MHC_Aa_ac[l].SetValue(i, j, # MHCtanh
-                            k0tmp / (np.tanh(-lmbda/MHC_erf_b) + 1))
+                    self.m.MHC_Aa_ac[l].SetValue(i, j, # MHCerf
+                            k0tmp / (spcl.erf(-lmbda/MHC_erf_b) + 1))
+#                    self.m.MHC_Aa_ac[l].SetValue(i, j, # MHCtanh
+#                            k0tmp / (np.tanh(-lmbda/MHC_erf_b) + 1))
                     self.m.scond_ac[l].SetValue(i, j,
                             D['scond_ac'][l] * (k*Tref)/(D['k0_ac'][l]*e*p_len**2))
                     self.m.Dsld_ac[l].SetValue(i, j,
@@ -1154,7 +1154,7 @@ class ERF(daeScalarExternalFunction):
         # the Derivative attribute of z will be non-zero. In that
         # case, return the derivative.
         if z.Derivative != 0:
-            res.Derivative = (2./np.sqrt(np.pi)) * np.exp(-zval**2)
+            res.Derivative = z.Derivative*(2./np.sqrt(np.pi)) * np.exp(-zval**2)
         return res
 
 class noise(daeScalarExternalFunction):