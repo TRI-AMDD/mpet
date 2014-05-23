2983d8115368e7134b769c3914d6db5eeb3e77e0
Raymond Smith <raymondbarrettsmith@gmail.com>
2014-05-23 11:21:29 -0400

    Update fit phi_eq function for graphite.
    This one actually looks like a staircase. Also, use built-in tanh
    instead of functions defined around built-in exp.

--- delta_phi_fits.py
+++ delta_phi_fits.py
@@ -20,31 +20,39 @@ class DPhiFits():
         """
         if self.D['Tabs'] != 298:
             raise NotImplementedError("Only fit for T = 298 K")
-        # XXX -- replace self.TANH, etc if possible
         del_phi_eq = self.eokT*(
-                4.19829 + 0.0565661*self.TANH(-14.5546*y + 8.60942) -
+                4.19829 + 0.0565661*np.tanh(-14.5546*y + 8.60942) -
                 0.0275479*(1/((0.998432 - y)**(0.492465)) - 1.90111) -
                 0.157123*np.exp(-0.04738*y**8) +
                 0.810239*np.exp(-40*(y - 0.133875))
                 ) - del_phi_ref
         return del_phi_eq
 
+#    def LiC6(self, y, del_phi_ref):
+#        """
+#        Fit \Delta\phi^{eq} for Li_y C_6 as a function of y.
+#        This can only return values for Tabs = 298 K
+#        This function was obtained from
+#        Doyle, Newman, 1996
+#        """
+#        if self.D['Tabs'] != 298:
+#            raise NotImplementedError("Only fit for T = 298 K")
+#        del_phi_eq = self.eokT*(-0.16 + 1.32*np.exp(-3.0*y) +
+#                10.*np.exp(-2000.*y)) - del_phi_ref
+#        return del_phi_eq
+
     def LiC6(self, y, del_phi_ref):
         """
         Fit \Delta\phi^{eq} for Li_y C_6 as a function of y.
         This can only return values for Tabs = 298 K
         This function was obtained from
-        Doyle, Newman, 1996
+        Safari, Delacourt 2011
         """
         if self.D['Tabs'] != 298:
             raise NotImplementedError("Only fit for T = 298 K")
-        del_phi_eq = self.eokT*(-0.16 + 1.32*np.exp(-3.0*y) +
-                10.*np.exp(-2000.*y)) - del_phi_ref
+        del_phi_eq = self.eokT*( 0.6379 + 0.5416*np.exp(-305.5309*y) +
+                0.044*np.tanh(-(y - 0.1958)/0.1088) -
+                0.1978*np.tanh((y - 1.0571)/0.0854) -
+                0.6875*np.tanh((y + 0.0117)/0.0529) -
+                0.0175*np.tanh((y - 0.5692)/0.0875)) - del_phi_ref
         return del_phi_eq
-
-    def SINH(self, y):
-        return 0.5*(np.exp(y) - np.exp(-y))
-    def COSH(self, y):
-        return 0.5*(np.exp(y) + np.exp(-y))
-    def TANH(self, y):
-        return self.SINH(y)/self.COSH(y)
