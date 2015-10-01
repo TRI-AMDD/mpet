bf94d9f5ed53ea11e4c812a26a78d619af38cc42
Raymond Smith <raymondbarrettsmith@gmail.com>
2015-09-30 21:42:29 -0400

    Fix elyte function to not recurse infinitely.

--- elyte_CST.py
+++ elyte_CST.py
@@ -23,19 +23,25 @@ def test1():
 #    N_A = self.N_A
 #    k = self.k
 #    e = self.e
+    """ Set of dilute solution parameters with zp=|zm|=nup=num=1,
+    Dp = 2.2e-10 m^2/s
+    Dm = 2.94e-10 m^2/s
+    """
+    Dp = 2.2e-10
+    Dm = 2.94e-10
     def D(c):
-        return 1e-10
+        return (2*Dp*Dm/(Dp+Dm))
     def thermFac(c):
         return 1.
     def tp0(c):
-        return 0.3
+        return Dp/(Dp+Dm)
     def kappa(c):
-        return D(c)*c*N_A*e**2/(k*Tref*(1-tp0(c)))
+        return Dm*c*N_A*e**2/(k*Tref*(1-tp0(c)))
     Dref = D(cref)
-    D = lambda c: D(c) / Dref
-    kappa = lambda c: kappa(c) * (
+    D_ndim = lambda c: D(c) / Dref
+    kappa_ndim = lambda c: kappa(c) * (
             k*Tref/(e**2*Dref*N_A*cref))
-    return D, kappa, thermFac, tp0, Dref
+    return D_ndim, kappa_ndim, thermFac, tp0, Dref
 
 def getProps(elytePropSet):
     if elytePropSet == "test1":
