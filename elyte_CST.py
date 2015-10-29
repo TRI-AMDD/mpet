7b266fa709d3add85e55d7daf385c8e0b36e8756
Raymond Smith <raymondbarrettsmith@gmail.com>
2015-10-29 13:58:31 -0400

    Add two elyte models (valoen/bernardi) and Fuller

--- elyte_CST.py
+++ elyte_CST.py
@@ -17,6 +17,62 @@ N_A = 6.022e23
 k = 1.381e-23
 e = 1.602e-19
 
+def LiClO4_PC():
+    """ Set of parameters from Fuller, Doyle, Newman 1994, with
+    conductivity directly from dualfoil5.2.f
+    """
+    def tp0(c):
+        return 0.2
+    def D(c):
+        return 2.58e-10 # m^2/s
+    def thermFac(c):
+        return 1.
+    def kappa(c):
+        p_max = 0.542
+        p_u = 0.6616
+        a = 0.855
+        b = -0.08
+        rho = 1.2041e3
+        out = 0.0001 + c**a * (
+                p_max*(1./(rho*p_u))**a *
+                np.exp(b*(c/rho - p_u)**2 -
+                (a/p_u)*(c/rho - p_u))) # S/m
+        return out
+    Dref = D(cref)
+    D_ndim = lambda c: D(c) / Dref
+    kappa_ndim = lambda c: kappa(c) * (
+            k*Tref/(e**2*Dref*N_A*(1000*cref)))
+    return D_ndim, kappa_ndim, thermFac, tp0, Dref
+
+def valoen_bernardi():
+    """ Set of parameters from Bernardi and Go 2011, indirectly from
+    Valoen and Reimers 2005
+    """
+    def tp0(c):
+        return 0.38
+    def D(c):
+        return 10**(-4) * 10**(-4.43 - 54/(Tref - (229 + 5*c)) - 0.22*c) # m^2/s
+    def thermFac(c):
+        tmp = 0.601 - 0.24*c**(0.5) + 0.982*(1 - 0.0052*(Tref - 294))*c**(1.5)
+        return tmp/(1-tp0(c))
+    def kappa(c):
+        (k00, k01, k02,
+                k10, k11, k12,
+                k20, k21) = (
+                -8.2488, 0.053248, -0.000029871,
+                0.26235, -0.0093063, 0.000008069,
+                0.22002, -0.0001765)
+        out = c * (k00 + k01*Tref + k02*Tref**2
+                + k10*c + k11*c*Tref + k12*c*Tref**2
+                + k20*c**2 + k21*c**2*Tref)**2 # mS/cm
+        out *= 0.1 # S/m
+        return out
+    Dref = D(cref)
+    D_ndim = lambda c: D(c) / Dref
+    kappa_ndim = lambda c: kappa(c) * (
+            k*Tref/(e**2*Dref*N_A*(1000*cref)))
+    return D_ndim, kappa_ndim, thermFac, tp0, Dref
+
 def test1():
 #    cref = self.cref
 #    Tref = self.Tref
@@ -30,21 +86,25 @@ def test1():
     Dp = 2.2e-10
     Dm = 2.94e-10
     def D(c):
-        return (2*Dp*Dm/(Dp+Dm))
+        return (2*Dp*Dm/(Dp+Dm)) # m^2/s
     def thermFac(c):
         return 1.
     def tp0(c):
         return Dp/(Dp+Dm)
     def kappa(c):
-        return Dm*c*N_A*e**2/(k*Tref*(1-tp0(c)))
+        return Dm*(1000*c)*N_A*e**2/(k*Tref*(1-tp0(c))) # S/m
     Dref = D(cref)
     D_ndim = lambda c: D(c) / Dref
     kappa_ndim = lambda c: kappa(c) * (
-            k*Tref/(e**2*Dref*N_A*cref))
+            k*Tref/(e**2*Dref*N_A*(1000*cref)))
     return D_ndim, kappa_ndim, thermFac, tp0, Dref
 
 def getProps(elytePropSet):
     if elytePropSet == "test1":
         return test1()
+    elif elytePropSet == "valoen_bernardi":
+        return valoen_bernardi()
+    elif elytePropSet == "LiClO4_PC":
+        return LiClO4_PC()
     else:
         raise NotImplementedError("Unrecognized elytePropSet")
