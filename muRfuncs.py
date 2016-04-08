2ba3a2c3ad16a3f5f19df66f48e983d162bf8442
Raymond Smith <raymondbarrettsmith@gmail.com>
2016-04-08 14:15:00 -0400

    Minor changes to calc_curv for clarity.

--- muRfuncs.py
+++ muRfuncs.py
@@ -327,18 +327,25 @@ def stepUp(x, xc, delta):
 def calc_curv(c, dr, r_vec, Rs, beta_s, particleShape):
     N = len(c)
     curv = np.empty(N, dtype=object)
+    # Here, beta_s = n*grad(c) at the surface.
+    # beta_s = (c_N - c_{N-2})/(2*dr)
+    # c_N = c_{N_2} + 2*dr*beta_s
     if particleShape == "sphere":
         curv[0] = 3 * (2*c[1] - 2*c[0]) / dr**2
         curv[1:N-1] = (np.diff(c, 2)/dr**2 +
-                (c[2:] - c[0:-2])/(dr*r_vec[1:-1]))
+                (2./r_vec[1:-1])*(c[2:] - c[0:-2])/(2*dr))
         curv[N-1] = ((2./Rs)*beta_s +
                 (2*c[-2] - 2*c[-1] + 2*dr*beta_s)/dr**2)
     elif particleShape == "cylinder":
         curv[0] = 2 * (2*c[1] - 2*c[0]) / dr**2
         curv[1:N-1] = (np.diff(c, 2)/dr**2 +
-                (c[2:] - c[0:-2])/(2 * dr*r_vec[1:-1]))
+                (1./r_vec[1:-1])*(c[2:] - c[0:-2])/(2*dr))
         curv[N-1] = ((1./Rs)*beta_s +
                 (2*c[-2] - 2*c[-1] + 2*dr*beta_s)/dr**2)
+#    elif particleShape == "rod":
+#        curv[0] = (2*c[1] - 2*c[0]) / dr**2
+#        curv[1:N-1] = np.diff(c, 2)/dr**2
+#        curv[N-1] = (2*c[-2] - 2*c[-1] + 2*dr*beta_s)/dr**2
     else:
         raise NotImplementedError("calc_curv_c only for sphere and cylinder")
     return curv
