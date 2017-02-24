71cce284aea264c74e76a3b43ab3ea901cb58725
Raymond Smith <raymondbarrettsmith@gmail.com>
2017-02-24 14:38:23 -0800

    Default to generating all plots for tests.
    Make a note in the readme that test failures aren't necessarily the
    worst.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -163,7 +163,7 @@ def show_fails(failList):
 
 
 def main(compareDir):
-    pflag = False
+    pflag = True
     dirDict = {}
     # Get the default configs
     dirDict["suite"] = osp.dirname(osp.abspath(__file__))
