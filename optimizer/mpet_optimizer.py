5cbbee801e4f7c6b09363851f5d5a95c7916a6d7
Debbie Zhuang <dezhuang@mit.edu>
2020-03-06 22:56:51 -0500

    added plot for optimal case

--- optimizer/mpet_optimizer.py
+++ optimizer/mpet_optimizer.py
@@ -32,7 +32,7 @@ area = float(sys.argv[5]) #in m^2
 #to the end of the parameter
 
 
-def f(inputs, pickle_file, A, params_sys, params_cathode, params_anode, params_list, f_handle):
+def f(inputs, expt_QV, A, params_sys, params_cathode, params_anode, params_list, f_handle):
     """The function that finds the residual of the input parameter values.
        Takes in the values of the parameters that we are optimizing in inputs.
        args is [pickle_file_name, area of electrode(m^2), params_sys, params_cathode, params_anode,
@@ -44,16 +44,12 @@ def f(inputs, pickle_file, A, params_sys, params_cathode, params_anode, params_l
     modify_mpet_params(params_sys, params_cathode, params_anode, params_list, inputs)
     main.main(params_sys) #runs simulation after subbing new parameters
     #run simulation, call plotter to get text file
-    if not os.path.exists(os.path.join(os.getcwd(), "sim_output")):
-        raise Exception("Input file doesn't exist")
     outmat2txt.main("sim_output")  #generates data
     general_data = np.loadtxt("sim_output/generalData.txt", delimiter = ",")
     #now saves data in general_data.txt
-    mpet_Q, mpet_V = calculate_VQ(general_data, A)
-    expt_Q, expt_V = process_experimental_data(pickle_file)
-    residual = area_difference(mpet_Q, mpet_V, expt_Q, expt_V) #find residual of plot area
-    output_string = "iteration: " + str(inputs) + "  residual:  " + str(residual) + '\n'
-    print(output_string)
+    mpet_Q, mpet_V = calculate_VQ(general_data, A) #generates MPET Q, V data
+    residual = area_difference(mpet_Q, mpet_V, expt_QV[:,0], expt_QV[:,1]) #find residual of plot area
+    output_string = "Iteration: " + str(inputs) + "  residual:  " + str(residual) + '\n'
     f_handle.write(output_string)
     return residual
 
@@ -70,24 +66,27 @@ def modify_mpet_params(params_sys, params_cathode, params_anode, params_to_modif
         param_name = params_to_modify[index]
         file_name = params_sys
         if params_to_modify[index][-7:] == "cathode":
+             #if in cathode, we modify the corresponding params
              param_name = params_to_modify[index][:-8]
              file_name = params_cathode
              #sed -i "s/$i*/c\$i = $j" params_sys
         elif params_to_modify[index][-5:] == "anode":
+             #if in anode, modify corresponding params
              param_name = params_to_modify[index][:-6]
              file_name = params_anode
         args = "sed -i '/" + param_name + " = "+ "*/c\\" + param_name + " = " + str(param_values[index]) + "' " + file_name
-        subprocess.run([args], shell = True)
+        subprocess.run([args], shell = True) #runs bash command through python
     return
 
 
 def calculate_VQ(data, A):
-    """Calculates the Q (in Ah/m^2), then multiplies by area to find the capacity.
+    """Calculates the Q (in Ah), then multiplies by area to find the capacity.
        Inputs are generalData np array from the results of our simulation, and the area in m^2
        Output is the array of voltages and capacities during filling"""
     V_array = data[:,3]
     time_array = data[:,0]
     I_array = data[:,5]
+    #Q = int(i*dt) * A in Ah/m^2 * m^2
     Q_array = integrate.cumtrapz(I_array, time_array, initial  = 0) * A
     return Q_array, V_array
 
@@ -106,26 +105,49 @@ def process_experimental_data(pickle_file_name):
     bat_dict = pickle.load(open(pickle_name, 'rb'))
     Q_array = bat_dict['b3c43']['cycles']['10']['Qd']
     V_array = bat_dict['b3c43']['cycles']['10']['V']
-    return Q_array, V_array
+    QV_array = np.vstack((Q_array, V_array))
+    print(QV_array.shape)
+    return QV_array
+
+
+def plot_functions(opt_args, expt_QV, A):
+    """Plots the final result of the data and the optimal function.
+       Inputs: optimal_arguments, data.
+       No outputs."""
+    #run simulation, call plotter to get text file
+    outmat2txt.main("sim_output")  #generates data
+    general_data = np.loadtxt("sim_output/generalData.txt", delimiter = ",")
+    #now saves data in general_data.txt
+    mpet_Q, mpet_V = calculate_VQ(general_data, A)
+    plt.plot(mpet_Q, mpet_V, 'r', expt_QV[:,0], expt_QV[:,1], 'b')
+    plt.legend(['MPET Simulation Fit', 'Experimental Data'])
+    plt.show()
+    return
 
 
 #tests to see if parameter file works!! 
-#try:
-#    main.main(mpet_params_sys)
-#except IndexError:
-#    print("ERROR: No parameter file specified. Aborting")
-#    raise
+try:
+    main.main(mpet_params_sys)
+except IndexError:
+    print("ERROR: No parameter file specified. Aborting")
+    raise
 
 #puts in initial guesses and parameters to tweak
 params_list = ['k0_cathode', 'k0_anode', 'alpha_cathode', 'alpha_anode', 'D_cathode', 'D_anode']
 x0 = [1.6e-1, 3.0e-2, 0.5, 0.5, 5.3e-19, 1.25e-12]
 
+#process and extract experimental data
+expt_QV_dat = process_experimental_data(pickle_name)
+
 #open iteration file
 f_handle = open('iteration_file', 'w')
 
-arg_list = [pickle_name, area, mpet_params_sys, mpet_params_cathode, mpet_params_anode, params_list, f_handle]
+#generates function arguments and variable bounds for parameters
+arg_list = [expt_QV_dat, area, mpet_params_sys, mpet_params_cathode, mpet_params_anode, params_list, f_handle]
 bnds = ((0, None), (0, None), (0, 1), (0, 1), (0, None), (0, None)) #bounds on parameters
 
-result = optimize.minimize(f, x0, args = tuple(arg_list), bounds = bnds, method = 'Nelder-Mead')
+result = optimize.minimize(f, x0, args = tuple(arg_list), bounds = bnds, method = 'L-BFGS-B')
 f_handle.close()
 print(result.success) # check if solver was successful
+
+plot_functions(results.inputs, expt_QV_dat, area)
