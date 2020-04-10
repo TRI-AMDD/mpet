TRI-AMDD/mpet

commit c391502b5b322e0a54fd7d27443d52406feee896

Adds an initialization case for CVsegments. Why is the initialization potential different than for CV?

mpet/sim.py
@@ -102,6 +102,8 @@ class SimMPET(dae.daeSimulation):
                 phi_guess=0
             elif ndD_s['profileType']=='CV':
                 phi_guess = self.ndD_s['Vset']-ndDVref
+            elif ndD_s['profileType']=='CVsegments':
+                phi_guess = self.ndD_s['segments'][0][0]
             else:
                 phi_guess = ndDVref
 

commit 551bbc9d8d4406f9fc6cf5faa278e9174e41bc0b

Fixes a mistake with CV initialization, introduced in c391502.

mpet/sim.py
@@ -101,7 +101,7 @@ class SimMPET(dae.daeSimulation):
             if ndD_s['tramp']>0:
                 phi_guess=0
             elif ndD_s['profileType']=='CV':
-                phi_guess = self.ndD_s['Vset']-ndDVref
+                phi_guess = self.ndD_s['Vset']
             elif ndD_s['profileType']=='CVsegments':
                 phi_guess = self.ndD_s['segments'][0][0]
             else: