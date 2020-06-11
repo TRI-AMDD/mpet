f541925f0098b06fcba80f90bff990ed978778f2
Debbie Zhuang <dezhuang@mit.edu>
2020-06-11 09:50:59 -0700

    added parameter files for cell2  and cell3 and electrolyte conductivity for EC:DMC 2:1

--- mpet/props_elyte.py
+++ mpet/props_elyte.py
@@ -57,6 +57,37 @@ def LiClO4_PC():
     return D_ndim, sigma_ndim, therm_fac, tp0, Dref
 
 
+def Doyle96_EC_DMC_2_1():
+    """ Set of parameters from Doyle, Newman, et al. 1996.
+    """
+    def tp0(c):
+        return 0.363
+
+    def D(c):
+        return 7.5e-11  # m^2/s
+
+    def therm_fac(c):
+        return 1.
+
+    def sigma(c):
+        r1=4.1253e-4
+        r2=5.007e-4 #This value in dualfoil differs from the paper
+        r3=4.7212e-4 #This value in dualfoil differs from the paper
+        r4=1.5094e-3
+        r5=1.6018e-4
+        k0 = r1 + r2*c - r3*c**2 + r4*c**3 - r5*c**4 #S/cm
+        return(100*k0)
+    
+    Dref = D(cref)
+
+    def D_ndim(c):
+        return D(c) / Dref
+
+    def sigma_ndim(c):
+        return sigma(c) * (
+            k*Tref/(e**2*Dref*N_A*(1000*cref)))
+    return D_ndim, sigma_ndim, therm_fac, tp0, Dref
+
 def Doyle96_EC_DMC_1_2():
     """ Set of parameters from Doyle, Newman, et al. 1996.
     """
@@ -87,6 +118,7 @@ def Doyle96_EC_DMC_1_2():
         return sigma(c) * (
             k*Tref/(e**2*Dref*N_A*(1000*cref)))
     return D_ndim, sigma_ndim, therm_fac, tp0, Dref
+
 
 
 def valoen_reimers():
