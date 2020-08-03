bff699c9d0d1fd63e125d42bb744504d1ff997f0
Dan Cogswell <cogswell@mit.edu>
2020-08-03 10:24:47 -0400

    The electrolyte conductivity and diffusivity for the isothermal simulation are different than for the non-isothermal simulations! This commit adds the functions for the isothermal case from the LIONSIMBA source code.

--- mpet/props_elyte.py
+++ mpet/props_elyte.py
@@ -194,3 +194,32 @@ def LIONSIMBA():
         return sigma(c) * (
             k*Tref/(e**2*Dref*N_A*(1000*cref)))
     return D_ndim, sigma_ndim, therm_fac, tp0, Dref
+
+
+def LIONSIMBA_isothermal():
+    """ Set of parameters from LIONSIMBA validation. Torchio et al, 2016.
+    """
+    T = 298 #isothermal model
+
+    def tp0(c):
+        return 0.364
+
+    def sigma(c):
+        ce = c*1000 #dimensionalized c
+        return 4.1253e-2 + 5.007e-4*ce - 4.7212e-7*ce**2 +1.5094e-10*ce**3 -1.6018*1e-14*ce**4 # S/m
+
+    def D(c):
+        return 7.5e-10 # m^2/s
+
+    def therm_fac(c):
+        return 1.
+
+    Dref = D(cref)
+
+    def D_ndim(c):
+        return D(c) / Dref
+
+    def sigma_ndim(c):
+        return sigma(c) * (
+            k*Tref/(e**2*Dref*N_A*(1000*cref)))
+    return D_ndim, sigma_ndim, therm_fac, tp0, Dref
