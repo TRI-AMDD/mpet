7156a428cfec37077c0fccb77880f25efac45e8e
Victor Azizi <victor@lipsum.eu>
2020-09-02 16:15:37 +0200

    fetch stable version before checking out

--- bin/mpetruntests.sh
+++ bin/mpetruntests.sh
@@ -14,6 +14,7 @@ coverage run --source=../mpet/ run_tests.py ../../bin/workdir/modified > /dev/nu
 COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN coveralls || true #Dont fret if if fails
 
 #run tests for stable branch
+git fetch origin $1:$1 #get stable branch before cloning
 git clone ../../ stable_branch/
 cd stable_branch/
 git checkout $1
