54ecbcf1458035f4b46e493f5ed5a18d665a1dd4
Victor Azizi <victor@lipsum.eu>
2020-09-11 19:07:04 +0200

    fetching and checking out a stable version simplified

--- bin/mpetruntests.sh
+++ bin/mpetruntests.sh
@@ -5,7 +5,8 @@ rm -rf workdir
 mkdir workdir
 cd workdir
 
-git fetch --all --tags --verbose #get stable branch before cloning
+git fetch --all --tags --verbose || : #get stable branch before cloning
+git clone ../../ stable_branch/ --branch=$1
 
 cp ../run_tests.py .
 ln -s ../../mpet .
@@ -13,13 +14,9 @@ ln -s ../../tests .
 
 #run tests for current commit
 coverage run --source=../../mpet/ run_tests.py ../../bin/workdir/modified > /dev/null
-COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN coveralls || true #Dont fret if if fails
+COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN coveralls || : #Dont fret if if fails
 
 #run tests for stable branch
-git clone ../../ stable_branch/
-cd stable_branch/
-git checkout $1
-cd ../
 ln -sf stable_branch/mpet .
 python run_tests.py ../../bin/workdir/stable > /dev/null
 
