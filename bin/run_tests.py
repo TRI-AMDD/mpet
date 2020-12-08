15e016d48fa290d58d96ea12e091c08d21121b9b
Victor Azizi <victor@lipsum.eu>
2020-12-08 15:19:57 +0100

    use argparse for argument sanity

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -1,17 +1,19 @@
 import os.path as osp
+import time
 from os import getcwd, makedirs,rmdir
 from sys import argv
 from tests.test_suite import run_test_sims, run_test_sims_analyt
 import tests.test_defs as defs
 from shutil import rmtree
+import argparse
 
-def run(dirName):
+def run(test_outputs, testDir):
     pflag = False
     dirDict = {}
     # Get the default configs
-    dirDict["suite"] = osp.join(osp.dirname(osp.abspath(__file__)),"tests")
+    dirDict["suite"] = testDir
     dirDict["simOut"] = osp.join(getcwd(), "sim_output")
-    dirDict["out"] = osp.join(dirDict["suite"], "test_outputs", dirName)
+    dirDict["out"] = test_outputs
     dirDict["baseConfig"] = osp.join(dirDict["suite"], "baseConfigs")
 
     # Dictionary containing info about the tests to run
@@ -34,4 +36,15 @@ def run(dirName):
     run_test_sims_analyt(runInfoAnalyt, dirDict)
 
 if __name__ == "__main__":
-  run(argv[1])
+  parser = argparse.ArgumentParser(description='Run test suite.')
+  parser.add_argument('--output_dir', metavar='o', type=str,
+      default=osp.join(osp.dirname(osp.abspath(__file__)),"tests",
+        "test_outputs", time.strftime("%Y%m%d_%H%M%S") ),
+                    help='where to put the simulations, default is tests/tests_outputs/date')
+  parser.add_argument('--test_dir', metavar='t', type=str, 
+      default=osp.join(osp.dirname(osp.abspath(__file__)),"tests"),
+                    help='where are the tests located?')
+  parser.add_argument('tests', nargs='*', default=[], help='which tests do I run?')
+  args = parser.parse_args()
+
+  run(args.output_dir,args.test_dir)
