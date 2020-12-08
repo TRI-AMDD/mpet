b002b8a8c562e51cccb50d0221f1ec7b5488d708
Victor Azizi <victor@lipsum.eu>
2020-12-08 16:48:06 +0100

    added more options to test cases with pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,14 +26,38 @@ def pytest_addoption(parser):
       default=2,
       help="tolerance for slowdown of test cases"
   )
+  parser.addoption(
+      "--tests",
+      action="store",
+      default=[],
+      nargs='*',
+      help="test cases to check"
+  )
+  parser.addoption(
+      "--skip-analytic", action="store_true", default=False, help="skip analytic tests"
+    )
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "analytic: mark test as analytic")
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--skip-analytic"):
+      skip_analytic = pytest.mark.skip(reason="--skip-analytic defined")
+      for item in items:
+        if "analytic" in item.keywords:
+          item.add_marker(skip_analytic)
+
 
 def pytest_generate_tests(metafunc):
   ntests = getNumberOfTests()
   if "Dirs" in metafunc.fixturenames:
     dir_t = metafunc.config.getoption("modDir")
     dir_b = metafunc.config.getoption("baseDir")
-    metafunc.parametrize("Dirs", [ (dir_b + '/test{:03}'.format(i),
-        dir_t + '/test{:03}'.format(i))for i in range(1, ntests+1)])
+    if metafunc.config.getoption("tests") == []: 
+      metafunc.parametrize("Dirs", [ (dir_b + '/test{:03}'.format(i),
+          dir_t + '/test{:03}'.format(i))for i in range(1, ntests+1)])
+    else:
+      metafunc.parametrize("Dirs", [ (dir_b + "/" + test, dir_t + "/" + test) for test in metafunc.config.getoption("tests")])
   if "tol" in metafunc.fixturenames:
     metafunc.parametrize("tol", [float(metafunc.config.getoption("tolerance"))])
   if "testDir" in metafunc.fixturenames:
