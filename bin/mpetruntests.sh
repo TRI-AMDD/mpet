bfd4aecf30381e392236d3d1a4745e1afa4983a1
Dan Cogswell <cogswell@mit.edu>
2020-12-23 22:20:12 -0500

    Adds missing --output_dir flag.

--- bin/mpetruntests.sh
+++ bin/mpetruntests.sh
@@ -17,10 +17,10 @@ ln -s ../../mpet .
 ln -s ../../tests .
 
 #run tests for current commit
-coverage run --source=../../mpet/ run_tests.py ../../bin/workdir/modified > /dev/null
+coverage run --source=../../mpet/ run_tests.py --output_dir ../../bin/workdir/modified > /dev/null
 COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN coveralls || : #Dont fret if if fails
 
 #run tests for stable branch
 ln -sf stable_branch/mpet .
-python run_tests.py ../../bin/workdir/stable > /dev/null
+python run_tests.py --output_dir ../../bin/workdir/stable > /dev/null
 
