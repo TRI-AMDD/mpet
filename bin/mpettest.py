754ec0c333fc01b8f31dd95c634ab1e1010c91df
Debbie Zhuang <dezhuang@mit.edu>
2021-01-21 22:57:01 -0800

    revert back to changes in original mpettest script

--- bin/mpettest.py
+++ bin/mpettest.py
@@ -9,12 +9,6 @@ import run_tests
 #Run the tests and save the input args
 args=run_tests.main()
 
-def silentremove(filename):
-    try:
-        os.remove(filename)
-    except OSError:
-        pass
-
 #Create the list of arguments for pytest
 pytest_args=["--baseDir="+osp.join(args.test_dir,"ref_outputs"),
              "--modDir="+args.output_dir,
@@ -26,11 +20,5 @@ if args.tests:
     pytest_args.append("--tests="+" ".join(args.tests))
     pytest_args.append("--skip-analytic")
 
-
 #Run pytest
 pytest.main(pytest_args)
-
-#remove files if they exist 
-silentremove("LA4_8rep.MWF") 
-silentremove("Short_PreDiag_000173.000") 
-silentremove("test_mwf_LA4.000")
