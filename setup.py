c7555462eca1b0c5df280502fa80e320989bd1dc
Leon Oostrum <l.oostrum@esciencecenter.nl>
2021-02-12 09:53:54 +0100

    Add flake8 code style check to regression tests

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     url='https://bitbucket.org/bazantgroup/mpet',
     packages=['mpet','mpet.plot','mpet.electrode'],
     install_requires=["numpy","scipy","matplotlib","pyQt5"],
-    extras_require = {'test':['pytest','coverage', 'coveralls','configparser','h5py']},
+    extras_require = {'test':['pytest','coverage', 'coveralls','configparser','h5py','flake8']},
     python_requires='>=3.5,<3.8',
     scripts=['bin/mpetrun.py','bin/mpetplot.py'],
     classifiers=[
