56dee03bcc58612d29af553a390d2e0d336b4d67
Debbie Zhuang <dezhuang@mit.edu>
2021-03-03 14:51:42 -0800

    modified way of reading hdf5/mat files in test case

--- tests/compare_tests.py
+++ tests/compare_tests.py
@@ -6,40 +6,15 @@ import tests.test_defs as defs
 import errno
 import h5py
 import pytest
+import mpet.utils as utils
 
 def test_compare(Dirs, tol):
   refDir, testDir = Dirs
-  testFailed = False
   newDir = osp.join(testDir, "sim_output")
   refDir = osp.join(refDir, "sim_output")
-  newDatah5 = False
-  refDatah5 = False
-  newDataFile = osp.join(newDir, "output_data.mat")
-  if not osp.exists(newDataFile):
-    newDataFile = osp.join(newDir, "output_data.hdf5")
-    newDatah5 = True
-  assert osp.exists(newDataFile), "neither output_data.{mat,hdf5} present"
-  refDataFile = osp.join(refDir, "output_data.mat")
-  if not osp.exists(refDataFile):
-    refDataFile = osp.join(refDir, "output_data.hdf5")
-    refDatah5 = True
-  assert osp.exists(refDataFile), "neither output_data.{mat,hdf5} present"
+  newData = utils.open_data_file(newDir + "/output_data")
+  refData = utils.open_data_file(refDir + "/output_data")
 
-  try:
-    if newDatah5:
-      newData = h5py.File(newDataFile,'r')
-    else:
-      newData = sio.loadmat(newDataFile)
-  except IOError as exception:
-    # If it's an error _other than_ the file not being there
-    assert exception.errno == errno.ENOENT, "IO error on opening file"
-    assert False, "File %s does not exist"%(newDataFile)
-    return
-        
-  if refDatah5:
-    refData = h5py.File(refDataFile,'r')
-  else:
-    refData = sio.loadmat(refDataFile)
   for varKey in (set(refData.keys()) & set(newData.keys())):
     # TODO -- Consider keeping a list of the variables that fail
 
@@ -49,8 +24,8 @@ def test_compare(Dirs, tol):
 
     #Compute the difference between the solution and the reference
     try:
-        varDataNew = newData[varKey]
-        varDataRef = refData[varKey]
+        varDataNew = utils.get_dict_key(newData, varKey)
+        varDataRef = utils.get_dict_key(refData, varKey)
         diffMat = np.abs(varDataNew - varDataRef)
     except ValueError:
         assert False, "Fail from ValueError"
@@ -63,6 +38,7 @@ def test_compare(Dirs, tol):
         "Fail from tolerance\nVariable failing: %s\nMax error:\
         %f"%(varKey,np.max(diffMat)) 
 
+
 @pytest.mark.analytic
 def test_cyldifn(testDir, tol):
   _test_analytic(testDir + "/testAnalytCylDifn" , tol, (defs.testAnalytCylDifn, defs.analytCylDifn))
@@ -73,28 +49,13 @@ def test_sphdifn(testDir, tol):
 
 def _test_analytic(testDir, tol, info):
   newDir = osp.join(testDir, "sim_output")
-  newDatah5 = False
-  newDataFile = osp.join(newDir, "output_data.mat")
-  if not osp.exists(newDataFile):
-    newDataFile = osp.join(newDir, "output_data.hdf5")
-    newDatah5 = True
-  assert osp.exists(newDataFile), "neither output_data.{mat,hdf5} present"
+  newData = utils.open_data_file(newDir + "/output_data")
 
   dD_s, ndD_s = IO.read_dicts(osp.join(newDir, "input_dict_system"))
   tmp = IO.read_dicts(osp.join(newDir, "input_dict_c"))
   dD_e = {}
   ndD_e = {}
   dD_e["c"], ndD_e["c"] = tmp
-  try:
-    if newDatah5:
-      newData = h5py.File(newDataFile,'r')
-    else:
-      newData = sio.loadmat(newDataFile)
-  except IOError as exception:
-    # If it's an error _other than_ the file not being there
-    assert exception.errno == errno.ENOENT, "IO error on opening file"
-    assert False, "File %s does not exist"%(newDataFile)
-    return
   t_ref = dD_s["t_ref"]
   L_part = dD_s["psd_len"]["c"][0,0]
   nx_part = ndD_s["psd_num"]["c"][0,0]
@@ -102,11 +63,8 @@ def _test_analytic(testDir, tol, info):
   # Skip first time point: analytical solution fails at t=0.
   t0ind = 2
   r0ind = 1
-  if newDatah5:
-    tvecA = newData["phi_applied_times"][t0ind:] * (t_ref/t_refPart)
-  else:
-    tvecA = newData["phi_applied_times"][0][t0ind:] * (t_ref/t_refPart)
-  cmat = newData["partTrodecvol0part0_c"]
+  tvecA = utils.get_dict_key(newData, "phi_applied_times")[t0ind:] * (t_ref/t_refPart)
+  cmat = utils.get_dict_key(newData, "partTrodecvol0part0_c")
   cmin, cmax = np.min(cmat), np.max(cmat)
   delC = cmax - cmin
   # Skip center mesh point and first time points:
