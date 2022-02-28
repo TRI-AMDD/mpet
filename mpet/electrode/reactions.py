da8e48bae4ed4f9340ca7ee3a709b5fcf5036c5f
Ombrini <P.Ombrini@tudelft.nl>
2022-02-28 09:31:44 +0100

    added act_lyte in reaction BV_Lim2016

--- mpet/electrode/reactions.py
+++ mpet/electrode/reactions.py
@@ -24,7 +24,7 @@ def BV_Lim2016(eta, c_sld, c_lyte, k0, E_A, T, act_R=None,
     if act_R is None:
         act_R = c_sld/(1-c_sld)
     gamma_ts = (1./(1-c_sld))
-    ecd = (3*k0*(c_sld**alpha)
+    ecd = (3*k0*act_lyte*(c_sld**alpha)
            * ((1-c_sld)**(1-alpha))/gamma_ts)
     Rate = ecd * np.exp(-E_A/T + E_A/1) * (np.exp(-alpha*eta/T) - np.exp((1-alpha)*eta/T))
     return Rate
