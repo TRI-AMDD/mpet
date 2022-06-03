702b4fdfa5de063414b1bf0da67da19b876b646f
Ombrini <P.Ombrini@tudelft.nl>
2022-06-03 15:15:08 +0200

    style checks

--- bin/create_ensamble.py
+++ bin/create_ensamble.py
@@ -15,32 +15,28 @@ ensamble = [
 keys = [vals[0] for vals in ensamble]
 val = [vals[1] for vals in ensamble]
 
-
 if __name__ == '__main__':
-   # Read in file
-  if len(sys.argv) < 2:
-    print("need the config file [python create_enamble.py <baseconfig>]")
-    exit(1)
-  cfg = configparser.ConfigParser()
-  cfg.optionxform = str
-  cfg.read(sys.argv[1])
+    # Read in file
+    if len(sys.argv) < 2:
+        print("need the config file [python create_enamble.py <baseconfig>]")
+        exit(1)
+    cfg = configparser.ConfigParser()
+    cfg.optionxform = str
+    cfg.read(sys.argv[1])
 
-  #Create all variations
-  combinations = list(itertools.product(*val))
+    # Create all variations
+    combinations = list(itertools.product(*val))
 
-  for combination in combinations:
-    params = dict(zip(keys,combination))
-    print(params)
+    for combination in combinations:
+        params = dict(zip(keys,combination))
+        print(params)
+        new_cfg = cfg
+        nicename = []
+        for key, val in params.items():
+            new_cfg[key[0]][key[1]] = val
+            nicename.append(key[1] + "=" + val)
 
-    new_cfg = cfg
-    
-    nicename = []
-    for key, val in params.items():
-      new_cfg[key[0]][key[1]] = val
-      nicename.append(key[1] + "=" + val)
-
-
-    # Write config 
-    cfg_dir = os.path.dirname(sys.argv[1])
-    with open(cfg_dir + "/" + "-".join(nicename) + ".cfg", "w") as f:
-      new_cfg.write(f)
+        # Write config
+        cfg_dir = os.path.dirname(sys.argv[1])
+        with open(cfg_dir + "/" + "-".join(nicename) + ".cfg", "w") as f:
+            new_cfg.write(f)
