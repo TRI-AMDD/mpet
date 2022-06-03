329bdf0c7967726fffb5f21012f0e346eee8e60b
Victor Azizi <victor@lipsum.eu>
2022-06-03 14:17:45 +0200

    added script for creating ensamble configs

---
+++ bin/create_ensamble.py
@@ -0,0 +1,43 @@
+import configparser
+import sys
+import itertools
+from copy import deepcopy
+
+#Values that need ensamble
+ensamble = [
+  [("Geometry","L_c")  , ["2","43"]],
+  [("Geometry","L_a")  , ["3","5","43"]],
+]
+
+#helpers
+keys = [ vals[0] for vals in ensamble]
+val = [ vals[1] for vals in ensamble ]
+
+
+if __name__ == '__main__':
+  # Read in file
+  if len(sys.argv) < 2:
+    print("need the config file [python create_enamble.py <baseconfig>]")
+    exit(1)
+  cfg = configparser.ConfigParser()
+  cfg.optionxform = str
+  cfg.read(sys.argv[1])
+
+  #Create all variations
+  combinations = list(itertools.product(*val))
+
+  for combination in combinations:
+    params = dict(zip(keys,combination))
+    print(params)
+
+    new_cfg = deepcopy(cfg)
+    
+    nicename = []
+    for key, val in params.items():
+      new_cfg[key[0]][key[1]] = val
+      nicename.append(key[1] + "=" + val)
+
+
+    # Write config 
+    with open("-".join(nicename) + ".cfg", "w") as f:
+      new_cfg.write(f)
