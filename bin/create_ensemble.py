538b542a1aff9e898cb57b1188e00a28b16c8d28
Laura Ootes <l.ootes@esciencecenter.nl>
2022-10-27 07:36:52 +0000

    fix typo in script name

---
+++ bin/create_ensemble.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import configparser
+import sys
+import itertools
+import os
+
+# Values that need ensemble
+ensemble = [
+    [("Geometry","L_c"), ["2","43"]],
+    [("Geometry","L_a"), ["3","5","43"]],
+]
+
+# helpers
+keys = [vals[0] for vals in ensemble]
+val = [vals[1] for vals in ensemble]
+
+if __name__ == '__main__':
+    # Read in file
+    if len(sys.argv) < 2:
+        print("need the config file [python create_enamble.py <baseconfig>]")
+        exit(1)
+    cfg = configparser.ConfigParser()
+    cfg.optionxform = str
+    cfg.read(sys.argv[1])
+
+    # Create all variations
+    combinations = list(itertools.product(*val))
+
+    for combination in combinations:
+        params = dict(zip(keys,combination))
+        print(params)
+        new_cfg = cfg
+        nicename = []
+        for key, val in params.items():
+            new_cfg[key[0]][key[1]] = val
+            nicename.append(key[1] + "=" + val)
+
+        # Write config
+        cfg_dir = os.path.dirname(sys.argv[1])
+        with open(cfg_dir + "/" + "-".join(nicename) + ".cfg", "w") as f:
+            new_cfg.write(f)
+else:
+    print('hoi')
