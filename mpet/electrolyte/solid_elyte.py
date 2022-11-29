9ab0278faba706f413d3aa7fb37f98e228930e7c
Leon Oostrum <l.oostrum@esciencecenter.nl>
2022-11-29 16:07:55 +0100

    Fix tp0

--- mpet/electrolyte/solid_elyte.py
+++ mpet/electrolyte/solid_elyte.py
@@ -5,11 +5,11 @@ def solid_elyte():
     """
     Solid Electrolyte version
     """
-    tp0 = 0.9999  # tp0 is constant but a callable must be returned
+    tp0 = 0.99999
     D = 1.19e-11  # m^2/s
 
     Ign1, sigma_ndim, thermFac, Ign3, Dref = valoen_reimers()
     D_ndim = D / Dref
 
     # D_ndim and tp0 are constants, but a callable must be returned
-    return lambda c: D_ndim, sigma_ndim, thermFac, lambda c: tp0, Dref
+    return lambda c: D_ndim, sigma_ndim, thermFac, tp0, Dref
