9e7a9499e843bdb3697eb0a65fb8403b76e124e5
Ombrini <P.Ombrini-1@tudelft.nl>
2022-11-29 14:26:08 +0100

    LTO updated

--- mpet/electrode/materials/LTO.py
+++ mpet/electrode/materials/LTO.py
@@ -6,7 +6,7 @@ def LTO(self, y, ybar, muR_ref, ISfuncs=None):
     Vasileiadis 2017
     """
     muRtheta = -self.eokT * 1.55
-    muRhomog = self.reg_sln(y, self.get_trode_param("Omega_a"), ISfuncs)
+    muRhomog = self.reg_sln(y, self.get_trode_param("Omega_a"))
     muRnonHomog = self.general_non_homog(y, ybar)
     muR = muRhomog + muRnonHomog
     actR = np.exp(muR / self.T)
