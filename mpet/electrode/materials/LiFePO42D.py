982035979f3cc391df915ab4ae02d4699bd3cfbd
Ombrini <P.Ombrini-1@tudelft.nl>
2022-12-12 13:00:33 +0100

    elastic constant in the code chem pot

--- mpet/electrode/materials/LiFePO42D.py
+++ mpet/electrode/materials/LiFePO42D.py
@@ -3,14 +3,43 @@ import numpy as np
 
 def LiFePO42D(self, c_mat, ybar, muR_ref):
     muRtheta = -self.eokT*3.422
-    T = 1.
-    cbar = ybar
-    B = self.get_trode_param("B")
     Omega = self.get_trode_param("Omega_a")
     kappax = self.get_trode_param("kappa")
     kappay = kappax*10
     # beta_s = self.get_trode_param("beta_s")
     # for allowing muR_ref to be calculated
+    # FePo4 elastic constants (GPa)
+    c11 = 175.9
+    c22 = 153.6
+    c33 = 135.0
+    c44 = 38.8
+    c55 = 47.5
+    c66 = 55.6
+    c13 = 54.0
+    c12 = 29.6
+    c23 = 19.6
+
+    Cij = np.zeros((6,6))
+    Cij[0,0] = c11
+    Cij[1,1] = c22
+    Cij[2,2] = c33
+    Cij[3,3] = c44
+    Cij[4,4] = c55
+    Cij[5,5] = c66
+    Cij[1,0] = c12
+    Cij[0,1] = c12
+    Cij[2,0] = c13
+    Cij[0,2] = c13
+    Cij[1,2] = c23
+    Cij[2,1] = c23
+    # strain
+    e1 = 0.0517
+    e2 = 0.0359
+    e3 = -0.0186
+    ej = np.array([e1, e2, e3, 0, 0, 0])
+    sigma_i = np.dot(Cij,ej)
+    print(sigma_i)
+
     if np.size(c_mat) == 1:
         y = c_mat
         muRhomog = self.reg_sln(y, self.get_trode_param("Omega_a"))
@@ -40,23 +69,18 @@ def LiFePO42D(self, c_mat, ybar, muR_ref):
         # if then works try to optmize the loops
         for i in range(1,Nx+1):
             curvy[0] = 2*c_mat_tmp[i,1] - 2*c_mat_tmp[i,0]
-            curvy[-1] = 2*c_mat_tmp[i,-2] - 2*c_mat_tmp[i,-1] \
-                # + 2*dys*beta_s*c_mat_tmp[i-1,-1]*(1-c_mat_tmp[i-1,-1])
+            curvy[-1] = 2*c_mat_tmp[i,-2] - 2*c_mat_tmp[i,-1]
+        # + 2*dys*beta_s*c_mat_tmp[i-1,-1]*(1-c_mat_tmp[i-1,-1])
             curvy[1:Ny-1] = np.diff(c_mat_tmp[i,:],2)
             for j in range(Ny):
                 y = c_mat_tmp[i,j]
-                muRhomog = T*np.log(y/(1-y)) + Omega*(1-2*y)
+                muRhomog = self.T*np.log(y/(1-y)) + Omega*(1-2*y)
                 muR_mat[i-1,j] = muRhomog
                 curvx = (c_mat_tmp[i-1,j] - c_mat_tmp[i,j]) - (c_mat_tmp[i,j] - c_mat_tmp[i+1,j])
-                # curvy[i-1,j] = (c_mat_tmp[i-1,j-1] - c_mat_tmp[i-1,j]) - (c_mat_tmp[i-1,j]
-                #                                                           - c_mat_tmp[i-1,j+1])
-                # curvy[i-1,-1] = 2*c_mat_tmp[i-1,-2] - 2*c_mat_tmp[i-1,-1]
-                # + 2*dys*beta_s*c_mat_tmp[i-1,-1]*(1-c_mat_tmp[i-1,-1])
-                # curv_en = -kappax*curvx/(dxs**2) - kappay*curvy/(dys**2)
                 curv_en = -kappax*curvx/(dxs**2) - kappay*curvy[j]/(dys**2)
                 muR_mat[i-1,j] += curv_en
-                muR_mat[i-1,j] += B*(y - cbar)
+                muR_mat[i-1,j] += self.get_trode_param("B")*(y - ybar)
         # print(muR_mat)
-        actR_mat = np.exp(muR_mat/T)
+        actR_mat = np.exp(muR_mat/self.T)
         muR_mat += muRtheta + muR_ref
         return muR_mat, actR_mat
