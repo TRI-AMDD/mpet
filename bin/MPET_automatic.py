15164c6158ef4321f21fa3d94d08870a61e85ad5
Ombrini <P.Ombrini-1@tudelft.nl>
2023-02-02 17:31:58 +0100

    mpet automatic works

--- bin/MPET_automatic.py
+++ bin/MPET_automatic.py
@@ -4,22 +4,22 @@ import subprocess
 
 def run_MPET(cwd, config):
     os.chdir(cwd)
-    subprocess.call(["python", cwd + "\mpet-dev-delft\\bin\mpetrun.py", config])
-
-
-currentDir = os.getcwd()
+    subprocess.call(["python", cwd + r"\bin\mpetrun.py", config])
 
 
 cwd = os.getcwd()
 os.chdir(cwd)
-os.chdir(cwd+"\mpet-dev-delft\configs\configs_memory")
-conf = open('configs.txt',"r")
+
+
+# os.chdir(r".\configs\configs_memory")
+
+conf = open('ensemble_parallel_configs.txt',"r")
 lines = conf.readlines()
 os.chdir(cwd)
 # print(cwd)
 for line in lines:
-    print(line[:-1])
-    run_MPET(cwd, (cwd + '\mpet-dev-delft\configs\configs_400p_stdCHR\\' + line[:-1]))
+    cwd = os.getcwd()
+    run_MPET(cwd, (cwd + r'\\' + line[:-1]))
 
 
 
--- bin/create_ensamble.py
+++ bin/create_ensamble.py
@@ -6,48 +6,69 @@ import itertools
 import os
 
 # Values that need ensamble
-ensamble = [
-    [("Particles","mean_c"), ["100e-9","300e-9"]],
-    [("Particles","stddev_c"), ["10e-9","100e-9"]],
-    [("Sim Params","relTol"), ["1e-5","1e-7"]],
-    [("Sim Params","absTol"), ["1e-5","1e-7"]],
-    [("Electrodes", "cathode"), ['D=1e-18-k0=2.cfg','D=1e-18-k0=2.5.cfg','D=1e-18-k0=3.cfg',
-                                 'D=5e-18-k0=2.cfg','D=5e-18-k0=2.5.cfg','D=5e-18-k0=3.cfg',
-                                 'D=50e-18-k0=2.cfg','D=50e-18-k0=2.5.cfg','D=50e-18-k0=3.cfg',
-                                 'D=500e-18-k0=2.cfg','D=50e-18-k0=2.5.cfg','D=500e-18-k0=3.cfg']]
-]
+# ensamble = [
+#     [("Particles","mean_c"), ["100e-9","300e-9"]],
+#     [("Particles","stddev_c"), ["10e-9","100e-9"]],
+#     [("Sim Params","relTol"), ["1e-5","1e-7"]],
+#     [("Sim Params","absTol"), ["1e-5","1e-7"]],
+#     [("Electrodes", "cathode"), ['D=1e-18-k0=2.cfg','D=1e-18-k0=2.5.cfg','D=1e-18-k0=3.cfg',
+#                                  'D=5e-18-k0=2.cfg','D=5e-18-k0=2.5.cfg','D=5e-18-k0=3.cfg',
+#                                  'D=50e-18-k0=2.cfg','D=50e-18-k0=2.5.cfg','D=50e-18-k0=3.cfg',
+#                                  'D=500e-18-k0=2.cfg','D=50e-18-k0=2.5.cfg','D=500e-18-k0=3.cfg']]
+# ]
 
 # ensamble = [
 #     [("Material","D"), ["1e-18","5e-18","50e-18","500e-18"]],
 #     [("Reactions","k0"), ["2","2.5","3"]],
 # ]
 
-# helpers
-keys = [vals[0] for vals in ensamble]
-val = [vals[1] for vals in ensamble]
+
+def ensemble_definitions():
+    # Values that need ensamble
+    ensamble = [[("Particles","mean_c"), ["100e-9","300e-9"]],
+                [("Particles","stddev_c"), ["10e-9","100e-9"]],
+                [("Sim Params","relTol"), ["1e-5","1e-7"]],
+                [("Sim Params","absTol"), ["1e-5","1e-7"]],
+                [("Electrodes", "cathode"), ['D=1e-18-k0=2.cfg','D=1e-18-k0=2.5.cfg','D=1e-18-k0=3.cfg',
+                                             'D=5e-18-k0=2.cfg','D=5e-18-k0=2.5.cfg','D=5e-18-k0=3.cfg',
+                                             'D=50e-18-k0=2.cfg','D=50e-18-k0=2.5.cfg','D=50e-18-k0=3.cfg',
+                                             'D=500e-18-k0=2.cfg','D=50e-18-k0=2.5.cfg','D=500e-18-k0=3.cfg']]
+                ]
+
+    # helpers
+    keys = [vals[0] for vals in ensamble]
+    val = [vals[1] for vals in ensamble]
+    return keys, val
+
+
+def create_ensemble(cff, keys=None, val=None):
+    with open('ensemble_parallel_configs.txt', "w") as ff:
+        if keys is None and val is None:
+            keys, val = ensemble_definitions()
+        cfg = configparser.ConfigParser()
+        cfg.optionxform = str
+        cfg.read(cff)
+        # Create all variations
+        combinations = list(itertools.product(*val))
+        for combination in combinations:
+            params = dict(zip(keys,combination))
+            new_cfg = cfg
+            nicename = []
+            for key, val in params.items():
+                new_cfg[key[0]][key[1]] = val
+                nicename.append(key[1] + "=" + val)
+
+            # Write config
+            cfg_dir = os.path.dirname(cff)
+            with open(cfg_dir + "/" + "-".join(nicename) + ".cfg", "w") as f:
+                new_cfg.write(f)
+                ff.write(str(cfg_dir + "/" + "-".join(nicename) + ".cfg\n"))
+    return
+
 
 if __name__ == '__main__':
     # Read in file
     if len(sys.argv) < 2:
         print("need the config file [python create_enamble.py <baseconfig>]")
         exit(1)
-    cfg = configparser.ConfigParser()
-    cfg.optionxform = str
-    cfg.read(sys.argv[1])
-
-    # Create all variations
-    combinations = list(itertools.product(*val))
-
-    for combination in combinations:
-        params = dict(zip(keys,combination))
-        print(params)
-        new_cfg = cfg
-        nicename = []
-        for key, val in params.items():
-            new_cfg[key[0]][key[1]] = val
-            nicename.append(key[1] + "=" + val)
-
-        # Write config
-        cfg_dir = os.path.dirname(sys.argv[1])
-        with open(cfg_dir + "/" + "-".join(nicename) + ".cfg", "w") as f:
-            new_cfg.write(f)
+    create_ensemble(sys.argv[1])
