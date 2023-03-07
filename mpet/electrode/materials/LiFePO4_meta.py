9ba1274b4d9c0cdc7b340bb4c7cbe858f945caaa
Ombrini <P.Ombrini-1@tudelft.nl>
2023-03-07 16:47:25 +0100

    matstable LFP

--- mpet/electrode/materials/LiFePO4_meta.py
+++ mpet/electrode/materials/LiFePO4_meta.py
@@ -5,10 +5,12 @@ def LiFePO4_meta(self, y, ybar, T, muR_ref):
     """ Bai, Cogswell, Bazant 2011 """
     muRtheta = -self.eokT*3.422
     muRhomog = 2*self.reg_sln(y, T, self.get_trode_param("Omega_a")/2)
-    a = -0.2
     b = 0.15
     pi = 3.14159
+    a = -0.9
     muR_meta = a*6*pi*np.cos(pi*y-b)*(np.sin(pi*y-b))**5
+    # a = -0.1
+    # muR_meta = a*4*pi*np.cos(pi*y-b)*(np.sin(pi*y-b))**3
     muRnonHomog = self.general_non_homog(y, ybar)
     muR = muRhomog + muRnonHomog + muR_meta
     actR = np.exp(muR/T)
