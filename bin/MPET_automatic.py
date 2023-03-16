384802bc3c2bbdd0316d906dd948a52c76edaf3b
Ombrini <P.Ombrini-1@tudelft.nl>
2023-03-16 12:10:18 +0100

    back up before branch change

--- bin/MPET_automatic.py
+++ bin/MPET_automatic.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import numpy as np
 
 
 def run_MPET(cwd, config):
@@ -11,15 +12,21 @@ cwd = os.getcwd()
 os.chdir(cwd)
 
 
-# os.chdir(r".\configs\configs_memory")
-
-conf = open('ensemble_parallel_configs.txt',"r")
-lines = conf.readlines()
+os.chdir(r".\configs\mark_diff_Crate")
+config_list = os.listdir()
+# print(config_list)
+config_I_want = np.array([])
+for config in config_list:
+    if config[:22] == 'params_system_NMC_Mark':
+        config_I_want = np.append(config_I_want,config)
+print(config_I_want)
+# conf = open('ensemble_parallel_configs.txt',"r")
+# lines = conf.readlines()
 os.chdir(cwd)
-# print(cwd)
-for line in lines:
+# # print(cwd)
+for line in config_I_want:
     cwd = os.getcwd()
-    run_MPET(cwd, (cwd + r'\\' + line[:-1]))
+    run_MPET(cwd, (cwd + r'\\' + r'configs\\mark_diff_Crate\\' + line))
 
 
 
