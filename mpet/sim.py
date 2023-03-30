957626c9b17a4adf67d8d5896aee1166c399c152
Ombrini <91598680+Ombrini@users.noreply.github.com>
2023-03-30 18:14:11 +0200

    back to all cs0 in 1var

--- mpet/sim.py
+++ mpet/sim.py
@@ -67,13 +67,7 @@ class SimMPET(dae.daeSimulation):
         if not config["prevDir"] or config["prevDir"] == "false":
             # Solids
             for tr in config["trodes"]:
-                cs0_ic = config['cs0'][tr]
-                # Guess initial filling fractions
-                self.m.ffrac[tr].SetInitialGuess(cs0_ic)
-                epsrnd = 0
-                rndavg = epsrnd*(np.random.rand(Nvol[tr],Npart[tr]) - 0.5)
-                rndavg -= np.mean(rndavg)
-                cs0_mat = cs0_ic*np.ones((Nvol[tr],Npart[tr]), dtype=object) + rndavg
+                cs0 = config['cs0'][tr]
                 for i in range(Nvol[tr]):
                     # Guess initial volumetric reaction rates
                     self.m.R_Vp[tr].SetInitialGuess(i, 0.0)
@@ -88,7 +82,6 @@ class SimMPET(dae.daeSimulation):
                     for j in range(Npart[tr]):
                         Nij = config["psd_num"][tr][i,j]
                         part = self.m.particles[tr][i,j]
-                        cs0 = cs0_mat[i,j]
                         # Guess initial value for the average solid
                         # concentrations and set initial value for
                         # solid concentrations
