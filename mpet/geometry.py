3857562991b6eefb3f49def72aa0073cc6bf9a50
Ombrini <91598680+Ombrini@users.noreply.github.com>
2023-03-30 18:12:33 +0200

    improve plate geometry

--- mpet/geometry.py
+++ mpet/geometry.py
@@ -40,8 +40,8 @@ def get_unit_solid_discr(Shape, N):
         dx = h / (N - 1)
         r_vec = np.linspace(0, h, N)
         vol_vec = L**2 * dx * np.ones(N)
-        vol_vec[0] = dx/2.
-        vol_vec[-1] = dx/2.
+        vol_vec[0] = L**2 * dx/2.
+        vol_vec[-1] = L**2 * dx/2.
         Vp = L**2 * h
         volfrac_vec = vol_vec / Vp
     else:
