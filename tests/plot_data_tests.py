00cf7b01a487d39893b1da21e0ac8f7de99b4a93
Dan Cogswell <cogswell@mit.edu>
2023-05-04 21:07:16 -0400

    Regression test to make all available plots and check for exceptions.

---
+++ tests/plot_data_tests.py
@@ -0,0 +1,18 @@
+import pytest
+import os.path as osp
+import matplotlib.pyplot as plt
+import mpet.plot.plot_data as plot_data
+
+plots = list(plot_data.plotTypes.keys())
+plots.remove("text")
+
+
+@pytest.mark.parametrize("plot", plots)
+def test_plot(Dirs, plot):
+    refDir, testDir = Dirs
+    try:
+        plot_data.show_data(osp.join(testDir, "sim_output"), plot, False, False, False)
+        plt.close()
+        assert True
+    except Exception:
+        assert False
