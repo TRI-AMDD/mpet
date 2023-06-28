efdeddf11edf12d95c3585722e274072ee26bac9
Ombrini <91598680+Ombrini@users.noreply.github.com>
2023-06-28 16:37:29 +0200

    Update main.py fixing times

--- mpet/main.py
+++ mpet/main.py
@@ -55,7 +55,8 @@ def run_simulation(config, outdir):
     # Set the time horizon and the reporting interval
     simulation.TimeHorizon = config["tend"]
     # The list of reporting times excludes the first index (zero, which is implied)
-    simulation.ReportingTimes = config["times"]
+    simulation.ReportingTimes = list(np.linspace(0, config["tend"], config["tsteps"] + 1))[1:]
+
 
     # Connect data reporter
     simName = simulation.m.Name + time.strftime(
